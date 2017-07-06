9387f58c6fe5122c9959873f94910b229e487f70
webber2408 <webcrat.tech@gmail.com>
2017-07-06 19:42:38 +0530

    home page complete by rahul

---
+++ js/about.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+  
+    $("#about").click(function(){ 
+      if($("#contact-div").is(":visible")){
+           $("#contact-div").slideToggle(500);
+      }
+      $("#about-div").slideToggle(500);
+   });
+    $("#contact").click(function(){ 
+        if($("#about-div").is(":visible")){
+           $("#about-div").slideToggle(500);
+        }
+        $("#contact-div").slideToggle(500);
+      
+   });
+});
