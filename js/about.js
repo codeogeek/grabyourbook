f5f44cdb2faaa0b42e5cfc1191225beb901ed1b8
webber2408 <webcrat.tech@gmail.com>
2017-07-10 01:26:43 +0530

    added carousel

--- js/about.js
+++ js/about.js
@@ -4,12 +4,19 @@ jQuery(document).ready(function($){
       if($("#contact-div").is(":visible")){
            $("#contact-div").slideToggle(500);
       }
+      else if($("#carousel-div").is(":visible")){
+              $("#carousel-div").slideToggle(500);
+      }
       $("#about-div").slideToggle(500);
    });
+
     $("#contact").click(function(){ 
         if($("#about-div").is(":visible")){
            $("#about-div").slideToggle(500);
         }
+        else if($("#carousel-div").is(":visible")){
+              $("#carousel-div").slideToggle(500);
+      }
         $("#contact-div").slideToggle(500);
       
    });
@@ -17,4 +24,22 @@ jQuery(document).ready(function($){
    $("#toggle").click(function(){
         $(".header-res").slideToggle(500);
    });
+
+    $("#recentToggle").click(function(){
+         if($("#about-div").is(":visible")){
+            $("#about-div").slideToggle(500);
+        }
+        else if($("#contact-div").is(":visible")){
+                 $("#contact-div").slideToggle(500);
+        }
+        
+        if($("#carousel-div").is(":visible")){
+            $("#recentToggle").innerHTML='Hide most selling!';
+        }
+        else{
+            $("#recentToggle").innerHTML="View most selling!";
+        }
+        $("#carousel-div").slideToggle(500);
+   });
+
 });
